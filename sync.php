2b77be72dc483cedec7c2306105f56106113d8d0
Antidote <antidotedefacer@gmail.com>
2019-02-06 13:07:50 +0300

    Frond End
    Improvement Done.

--- sync.php
+++ sync.php
@@ -133,7 +133,7 @@ switch (@$_GET['step']) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
  
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css"  >
+    <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css"  >
   </head>
   <body  >
   
